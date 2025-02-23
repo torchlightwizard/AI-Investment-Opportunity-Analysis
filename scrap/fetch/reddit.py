47196e5926198f00f473f11f9dc28d40a6f77679
Torch <rustruedetective@gmail.com>
2025-02-23 21:15:41 +0500

    started reddit api

---
+++ scrap/fetch/reddit.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv as env
+from datetime import datetime
+import os
+import praw
+import json
+
+path_to_keys = "./keys/.env"
+env(path_to_keys)
+
+auth_params = {
+    "client_id": f"{os.getenv('REDDIT_client_id')}",
+    "client_secret": f"{os.getenv('REDDIT_client_secret')}",
+    "user_agent": f"{os.getenv('REDDIT_USER_AGENT')}",
+    "username": f"{os.getenv('REDDIT_USERNAME')}",
+    "password": f"{os.getenv('REDDIT_PASSWORD')}"
+    }
+subreddits = ["MachineLearning"]
+search_query = "ai fund"
+output_folder_path = "./scrap/data/reddit/"
+post_ids = ["", "", "", "", "", ""]
+
+
+
+def write_response (output_folder_path, file_name, data):
+    os.makedirs(output_folder_path, exist_ok=True)
+    time_stamp = datetime.now().strftime(('%Y_%m_%dT%H_%M_%S'))
+    output_file_path = os.path.join(output_folder_path, f"{file_name}_{time_stamp}.json")
+    with open(output_file_path, "w") as file:
+        json.dump(data, file, indent=4)
+    return data
+
+
+
+def auth (client_id, client_secret, user_agent, username, password):
+    """
+        Return:
+            Reddit Object: praw.reddit.Reddit
+    """
+    try:
+        reddit = praw.Reddit(
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent=user_agent,
+            username=username,
+            password=password
+        )
+        return reddit
+    except Exception as err:
+        print(f"Failed to intialize reddit.")
+        print(f"Unexpected Error: {err}")
+    return None
