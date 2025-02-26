9e9d1f3db899891c9ef63a5e8dc8bc37ff5eee57
Torch <rustruedetective@gmail.com>
2025-02-26 21:30:55 +0500

    added outline

---
+++ docs/Outline.md
@@ -0,0 +1,49 @@
+# Intro
+This page outlines the scope of this project from beginning till end and maps how different components contribute to the analysis.
+
+<br><br><br>
+
+
+
+# Data
+We will be sourcing our data primarily through apis and past data sources.
+The apis will help us for new data predictions.
+The past data will be used for training.
+
+We will use these sources for our data:<br>
+1. Youtube API
+2. Reddit API (Prawn)
+3. Google Search
+4. News Data API - https://newsdata.io/
+5. News API - https://newsapi.org/
+6. Market Aux API - https://www.marketaux.com/
+7. News DataHub API - https://newsdatahub.com/
+    Their servers are down, will reattempt after they are live.
+8. Pyprojects - https://pypi.org/project/pytrends/
+    Not an official api and does not work without proper proxy, session and header setup. Hence not used.
+9. Newspaper3k - https://newspaper.readthedocs.io/en/latest/
+    Not an official api and does not work without proper proxy, session and header setup. Hence not used.
+
+## Extract
+
+## Transform
+
+## Load
+
+<br><br><br>
+
+
+
+# Preprocessing
+
+<br><br><br>
+
+
+
+# Analysis
+
+<br><br><br>
+
+
+
+# Prediction
--- scrap/fetch/youtube_oauth.py
+++ scrap/fetch/youtube_oauth.py
@@ -14,6 +14,7 @@ load_dotenv(path_to_keys)
 path_to_secret = f"{os.getenv('PATH_TO_SECRET')}"
 path_to_token = f"{os.getenv('YPATH_TO_TOKEN')}"
 port = f"{os.getenv('PORT')}"
+limit=10
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 video_id = ""
@@ -41,6 +42,9 @@ def get_creds (path_to_secret, path_to_token, scopes, port):
     """
 
     try:
+        if not all(isinstance(arg, str) for arg in [path_to_secret, path_to_token]):
+            raise TypeError("path_to_secret, path_to_token arguments must be strings.")
+
         port = int(port)
         creds = None
         if os.path.exists(path_to_token):
@@ -51,7 +55,7 @@ def get_creds (path_to_secret, path_to_token, scopes, port):
                     with open(path_to_token, "w") as token:
                         token.write(creds.to_json())
                 except GoogleAuthError as refresh_err:
-                    print("Token refresh failed, re-authenticating:", refresh_err)
+                    print(f"Token refresh failed, re-authenticating:", refresh_err)
 
         if not creds or not creds.valid:
             flow = InstalledAppFlow.from_client_secrets_file(path_to_secret, scopes)
@@ -61,10 +65,14 @@ def get_creds (path_to_secret, path_to_token, scopes, port):
         return creds
     
     except GoogleAuthError as err:
-        print(f"Google Authentication Error: {err}")
+        print(f"Function: get_creds. Google Authentication Error: {err}")
+    except ValueError as err:
+        print(f"Function: get_creds. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_creds. Key Error: {err}")
     except Exception as err:
-        print(f"Unexpected Error: {err}")
-    return 
+        print(f"Function: get_creds. Unexpected Error: {err}")
+    return None
 
 
 
@@ -79,9 +87,13 @@ def get_youtube (credentials):
     try:
         youtube = build("youtube", "v3", credentials=credentials)
         return youtube
+    except ValueError as err:
+        print(f"Function: get_youtube. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_youtube. Key Error: {err}")
     except Exception as err:
-        print(f"Unexpected Error: {err}")
-        return None
+        print(f"Function: get_youtube. Unexpected Error: {err}")
+    return None
 
 
 
@@ -92,15 +104,22 @@ def auth (path_to_secret, path_to_token, scopes, port):
         Returns:
             Resource Object: googleapiclient.discovery.Resource
     """
-
-    creds = get_creds(path_to_secret, path_to_token, scopes, port)
-    if creds is None:
-        print("Credentials creation failed.")
-        return None
-    youtube = get_youtube(creds)
-    if youtube is None:
-        print("Resource creation failed.")
-    return youtube
+    try:
+        creds = get_creds(path_to_secret, path_to_token, scopes, port)
+        if creds is None:
+            raise RuntimeError("Failed to authenticate. Credentials creation failed.")
+            return None
+        youtube = get_youtube(creds)
+        if youtube is None:
+            raise RuntimeError("Failed to authenticate. Resource creation failed.")
+        return youtube
+    except ValueError as err:
+        print(f"Function: auth. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: auth. Key Error: {err}")
+    except Exception as err:
+        print(f"Function: auth. Unexpected Error: {err}")
+    return None
 
 
 
@@ -111,17 +130,213 @@ def get_video_statistics (path_to_secret, path_to_token, scopes, port, output_fo
         Returns:
             Response Object: Video Statistics | {viewCount, likeCount, commentCount}
     """
-        
-    youtube = auth(path_to_secret, path_to_token, scopes, port)
-    if youtube is None:
-        return None
     
-    req = youtube.videos().list(part="statistics", id=video_id)
-    res = req.execute()
-    if "items" not in res or not res["items"]:
-        raise ValueError("Invalid response. No items found.")
-    data = res["items"][0]["statistics"]
-    data = {key:value for key, value in data.items() if key in ["viewCount", "likeCount", "commentCount"]}
+    try:
+        if not all(isinstance(arg, str) for arg in [path_to_secret, path_to_token, output_folder_path, video_id]):
+            raise TypeError("path_to_secret, path_to_token, output_folder_path, video_id arguments must be strings.")
+        
+        youtube = auth(path_to_secret, path_to_token, scopes, port)
+        if youtube is None:
+            raise RuntimeError("Failed to authenticate. Check Credentials")
+        
+        req = youtube.videos().list(part="statistics", id=video_id)
+        res = req.execute()
+        if "items" not in res or not res["items"]:
+            raise ValueError("Invalid response. No items found.")
+        items = res.get("items", None)
+        first_item = items[0] if isinstance(items, list) else None
+        statistics = first_item.get("statistics", None) if isinstance(first_item, dict) else None
+        data = {key:value for key, value in statistics.items() if key in ["viewCount", "likeCount", "commentCount"]}
 
-    file_name = f"video_statistics_{video_id}"
-    return write_response(output_folder_path, file_name, data)
+        file_name = f"video_statistics_{video_id}"
+        return write_response(output_folder_path, file_name, data)
+    except ValueError as err:
+        print(f"Function: get_video_statistics. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_video_statistics. Key Error: {err}")
+    except Exception as err:
+        print(f"Function: get_video_statistics. Unexpected Error: {err}")
+    return None
+
+
+def get_video_details (path_to_secret, path_to_token, scopes, port, output_folder_path, video_id):
+    """
+        Fetches video details from response["items"][0]["snippet"] and saves to a JSON file.
+
+        Returns:
+            Response Object: Video Details | {title, description, publishedAt, channelId, channelTitle, categoryId}
+    """
+
+    try:
+        if not all(isinstance(arg, str) for arg in [path_to_secret, path_to_token, output_folder_path, video_id]):
+            raise TypeError("path_to_secret, path_to_token, output_folder_path, video_id arguments must be strings.")
+
+        youtube = auth(path_to_secret, path_to_token, scopes, port)
+        if youtube is None:
+            raise RuntimeError("Failed to authenticate. Check Credentials")
+        
+        req = youtube.videos().list(part="snippet", id=video_id)
+        res = req.execute()
+        if "items" not in res or not res["items"]:
+            raise ValueError("Invalid response. No items found.")
+        items = res.get("items", None)
+        first_item = items[0] if isinstance(items, list) else None
+        snippet = first_item.get("snippet", None) if isinstance(first_item, dict) else None
+        data = {key:value for key, value in snippet.items() if key in ["title", "description", "publishedAt", "channelId", "channelTitle", "categoryId"]}
+
+        file_name = f"video_details_{video_id}"
+        return write_response(output_folder_path, file_name, data)
+    except ValueError as err:
+        print(f"Function: get_video_details. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_video_details. Key Error: {err}")
+    except Exception as err:
+        print(f"Function: get_video_details. Unexpected Error: {err}")
+    return None
+
+
+
+def get_videos (path_to_secret, path_to_token, scopes, port, output_folder_path, search_query, limit=limit):
+    """
+        Fetches top videos after search from response["items"][i] and saves to a JSON file.
+
+        Returns:
+            Response Object: Video Ids | {id[videoId]}
+    """
+
+    try:
+        if not all(isinstance(arg, str) for arg in [path_to_secret, path_to_token, output_folder_path, search_query]):
+            raise TypeError("path_to_secret, path_to_token, output_folder_path, search_query arguments must be strings.")
+
+        youtube = auth(path_to_secret, path_to_token, scopes, port)
+        if youtube is None:
+            raise RuntimeError("Failed to authenticate. Check Credentials")
+        
+        yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
+        req = youtube.search().list(
+            part="snippet",
+            q=search_query,
+            type="video",
+            videoDuration="medium",
+            maxResults=limit,
+            order="date",
+            relevanceLanguage="en",
+            publishedAfter=yesterday
+        )
+        res = req.execute()
+        if "items" not in res or not res["items"]:
+                raise ValueError("Invalid response. No items found.")
+        items = res.get("items", [])
+        data = []
+        for item in items:
+            data.append({
+                "id": item["id"]["videoId"],
+                "title": item["snippet"]["title"],
+                "description": item["snippet"]["description"],
+                "publishedAt": item["snippet"]["publishedAt"],
+                "channelId": item["snippet"]["channelId"]
+            })
+
+        file_name = f"top_videos"
+        return write_response(output_folder_path, file_name, data)
+    except ValueError as err:
+        print(f"Function: get_videos. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_videos. Key Error: {err}")
+    except Exception as err:
+        print(f"Function: get_videos. Unexpected Error: {err}")
+    return None
+
+
+
+def get_comments (path_to_secret, path_to_token, scopes, port, output_folder_path, video_id, limit=limit):
+    """
+        Fetches top comments of a video from response["items"][i]["snippet"] and saves to a JSON file.
+
+        Returns:
+            Response Object: Video Top Comments and Statistics | {topLevelComment[snippet[textOriginal, likeCount, publishedAt]], totalReplyCount}
+    """
+
+    try:
+        if not all(isinstance(arg, str) for arg in [path_to_secret, path_to_token, output_folder_path, video_id]):
+            raise TypeError("path_to_secret, path_to_token, output_folder_path, video_id arguments must be strings.")
+        
+        youtube = auth(path_to_secret, path_to_token, scopes, port)
+        if youtube is None:
+            raise RuntimeError("Failed to authenticate. Check Credentials")
+        
+        req = youtube.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            maxResults=limit,
+            order="relevance"
+        )
+        res = req.execute()
+        if "items" not in res or not res["items"]:
+            raise ValueError("Invalid response. No items found.")
+        items = res.get("items", [])
+        data = []
+        for item in items:
+            item_snippet = item["snippet"]["topLevelComment"]["snippet"]
+            data.append({
+                "textOriginal": item_snippet["textOriginal"],
+                "likeCount": item_snippet["likeCount"],
+                "publishedAt": item_snippet["publishedAt"],
+                "totalReplyCount": item["snippet"]["totalReplyCount"]
+            })
+
+        file_name = f"top_comments_{video_id}"
+        return write_response(output_folder_path, file_name, data)
+    except ValueError as err:
+        print(f"Function: get_comments. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_comments. Key Error: {err}")
+    except Exception as err:
+        print(f"Function: get_comments. Unexpected Error: {err}")
+    return None
+
+
+
+def get_categories (path_to_secret, path_to_token, scopes, port, output_folder_path):
+    """
+        Fetches all categories from response["items"][i] and saves to a JSON file.
+
+        Returns:
+            Response Object: All Categories | {id, snippet[title]} | 
+    """
+        
+    try:
+        if not all(isinstance(arg, str) for arg in [path_to_secret, path_to_token, output_folder_path]):
+            raise TypeError("path_to_secret, path_to_token, output_folder_path arguments must be strings.")
+        
+        youtube = auth(path_to_secret, path_to_token, scopes, port)
+        if youtube is None:
+            raise RuntimeError("Failed to authenticate. Check Credentials")
+        
+        req = youtube.videoCategories().list(part="snippet", regionCode="US")
+        res = req.execute()
+        if "items" not in res or not res["items"]:
+            raise ValueError("Invalid response. No items found.")
+        items = res.get("items", [])
+        data = []
+        for item in items:
+            data.append({
+                "id": item["id"],
+                "title": item["snippet"]["title"]
+            })
+
+        file_name = f"categories"
+        return write_response(output_folder_path, file_name, data)
+    except ValueError as err:
+        print(f"Function: get_categories. Value Error: {err}")
+    except KeyError as err:
+        print(f"Function: get_categories. Key Error: {err}")
+    except Exception as err:
+        print(f"Function: get_categories. Unexpected Error: {err}")
+    return None
+
+# get_video_statistics(path_to_secret, path_to_token, scopes, port, output_folder_path, video_id)
+# get_video_details(path_to_secret, path_to_token, scopes, port, output_folder_path, video_id)
+# get_videos(path_to_secret, path_to_token, scopes, port, output_folder_path, search_query)
+# get_comments(path_to_secret, path_to_token, scopes, port, output_folder_path, video_id)
+# get_categories(path_to_secret, path_to_token, scopes, port, output_folder_path)
