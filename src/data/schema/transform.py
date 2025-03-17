2fa38571493b4813979827ebcf4680ff05883f7e
Torch <rustruedetective@gmail.com>
2025-03-18 00:53:46 +0500

    completed schema

---
+++ src/data/schema/transform.py
@@ -0,0 +1,75 @@
+youtube = {
+    "company": "",
+    "search_query": "",
+    "video_id": "",
+    "video_title": "",
+    "video_description": "",
+    "channel_id": "",
+    "channel_title": "",
+    "category": "",
+    "views": "",
+    "likes": "",
+    "comments": ""
+}
+
+youtube_comments = {
+    "id": "",
+    "video_id": "",
+    "text": "",
+    "likes": "",
+    "replies": ""
+}
+
+reddit = {
+    "company": "",
+    "search_query": "",
+    "id": "",
+    "subreddit_id": "",
+    "title": "",
+    "description": "",
+    "comments": "",
+    "score": "",
+    "upvote_ratio": ""
+}
+
+reddit_comments = {
+    "company": "",
+    "search_query": "",
+    "id": "",
+    "post_id": "",
+    "text": "",
+    "score": ""
+}
+
+news_marketaux = {
+    "company", "",
+    "search_query", "",
+    "source", "",
+    "title", "",
+    "description", "",
+    "sentiment", ""
+}
+
+news_dataio = {
+    "company", "",
+    "search_query", "",
+    "source", "",
+    "title", "",
+    "description", ""
+}
+
+news_apiorg = {
+    "company": "",
+    "search_query": "",
+    "source": "",
+    "title": "",
+    "description": ""
+}
+
+googlesearch = {
+    "company", "",
+    "search_query", "",
+    "title", "",
+    "search_results", "",
+    "format_time", ""
+}
