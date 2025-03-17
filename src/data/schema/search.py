0adc6ed50c4b3984aa57ec4d7c8efc61f55b763c
Torch <rustruedetective@gmail.com>
2025-03-18 00:53:19 +0500

    added search schema

---
+++ src/data/schema/search.py
@@ -0,0 +1,34 @@
+search_db = {
+    "company": {
+        "name": "",
+        "stock_indicator": "",
+    },
+    "positive": {
+        "keys": "",
+    },
+    "negative": {
+        "keys": ""
+    }
+}
+
+
+
+search_json = {
+    "company": [
+        {
+            "name": "",
+            "stock_indicator": ""
+        }
+    ],
+    "keys": {
+        "youtube": [],
+        "reddit": {
+            "subreddits": [],
+            "queries": []
+        },
+        "googlesearch": [],
+        "news_apiorg": [],
+        "news_dataio": [],
+        "news_marketaux": []
+    }
+}
